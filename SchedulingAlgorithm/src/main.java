3f44b601edd2ce28c9d4c1b0c1566ccbadd3f9df
unique91 <jsaldan38@gmail.com>
2019-12-06 11:07:19 +0100

    add scanner and menü

--- SchedulingAlgorithm/src/main.java
+++ SchedulingAlgorithm/src/main.java
@@ -11,7 +11,34 @@ public class main {
 		System.out.println(" __________________________________ ");
 
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Choose your Process size = ");
+		int n = sc.nextInt();
 		
+		char c = 'A';
+		char[] procName = new char[n];
+		int[] process = new int[n];
+		System.out.println("Process | Burst-Time");
+		for(int i = 0; i < n; i++) {
+			procName[i] = c;
+			System.out.print("  " + c++ + "           ");
+			process[i] = sc.nextInt();
+		}
+		
+		System.out.print("Choose your Algorithm ");
+		int choose = sc.nextInt();
+		
+		System.out.println();
+		switch (choose) {
+		case 1:
+			System.out.println("****** Your choice was FCFS ******");
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+		}
 	}
 
 }
