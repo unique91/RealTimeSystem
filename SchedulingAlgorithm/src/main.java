2c811161c8606c338d62e72663d669a773197c66
unique91 <jsaldan38@gmail.com>
2019-12-06 10:41:16 +0100

    complete the menü

---
+++ SchedulingAlgorithm/src/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println(" __________________________________ ");
+		System.out.println("| **** Choose your Algorithm ***** |");
+		System.out.println("| 1. First Come First Serve (FCFS) |");
+		System.out.println("| 2. Shorted Job First (SJF)       |");
+		System.out.println("| 3. Round Robin (RR)              |");
+		System.out.println(" __________________________________ ");
+
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
